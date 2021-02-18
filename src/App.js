1188351337591ec0b838f2a5a9bd5fa5d26e990a
Bader Al-Ramadan <bader.ram@gmail.com>
2021-02-18 15:54:01 +0300

    shop test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+//data
+import tshirts from "/Users/baderal-ramadan/Desktop/Development/React/shop/src/products.js"
+//styling
+import styles from "/Users/baderal-ramadan/Desktop/Development/React/shop/src/styles.js";
+import img from "/Users/baderal-ramadan/Desktop/Development/React/shop/src/MoonLight.png"
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <title>MoonLight</title>
+        <img src={img} className="Moon" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Add light to your <b>world</b> 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <div style={styles.shirt}>
+  <img
+    style={styles.shirtImage}
+    alt={tshirts[0].name}
+    src={tshirts[0].image}
+  />
+  <p style={styles.text}>{tshirts[0].name}</p>
+  <p style={styles.text}>{tshirts[0].price} KD</p>
+</div>
+
+
     </div>
   );
 }
-
+console.log(tshirts[0].image)
 export default App;
---
+++ src/products.js
@@ -0,0 +1,19 @@
+import tshirt1 from "/Users/baderal-ramadan/Desktop/Development/React/shop/src/img/Tshirt 2.jpg"
+import tshirt2 from "/Users/baderal-ramadan/Desktop/Development/React/shop/src/img/Tshirt.png"
+
+
+const tshirts = [
+    {
+      name: "Moon Light",
+      price: 10,
+      image:  "/Users/baderal-ramadan/Desktop/Development/React/shop/src/img/Tshirt 2.jpg",
+    },
+    {
+      name: "Chemex Brew",
+      price: 11,
+      image:{tshirt2},
+    },
+
+  ];
+  console.log(tshirts[0].image)
+  export default tshirts;
